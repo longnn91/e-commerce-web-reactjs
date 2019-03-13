64fc1fd592e4c28420352cc9cae28bfc2f117431
long.nn@neo-lab.vn <long.nn@neo-lab.vn>
2019-03-13 17:12:17 +0700

    add basic component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import NavBar from './components/NavBar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+          <NavBar></NavBar>
+          <Switch>
+              <Route path='/' exact="true" component={ProductList} />
+              <Route path='/details' component={Details} />
+              <Route path='/cart' component={Cart} />
+              <Route path='' component={Default} />
+          </Switch>
+      </React.Fragment>
     );
   }
 }
